96fc94a69374e751edeb4aa0f13da11363414e02
bwjson <bwjson27@gmail.com>
2025-03-30 16:37:37 +0500

    FIX URLS 2

--- internal/transport/handler.go
+++ internal/transport/handler.go
@@ -23,7 +23,13 @@ func NewHandler(services *service.Services, repo *repository.Repos, s3 *pkg.S3Cl
 func (h *Handler) InitRoutes() *gin.Engine {
 	r := gin.Default()
 
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
