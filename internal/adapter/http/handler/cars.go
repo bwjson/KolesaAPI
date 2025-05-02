6f0993d9e36cbe01436d822ebd8250e06b7e99d4
bwjson <bwjson27@gmail.com>
2025-05-02 18:08:33 +0500

    CHECK v2

--- internal/adapter/http/handler/cars.go
+++ internal/adapter/http/handler/cars.go
@@ -237,7 +237,13 @@ func (h *Handler) Create(c *gin.Context) {
 		response.NewErrorResponse(c, http.StatusBadRequest, err.Error())
 	}
 
-	h.services.Cars.Create(ctx, car)
+	zero, err := h.services.Cars.Create(ctx, car)
+	if err != nil {
+		response.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	response.NewSuccessResponse(c, http.StatusOK, zero)
 }
 
 func (h *Handler) UpdateById(c *gin.Context) {}
